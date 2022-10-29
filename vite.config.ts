ccafd7fb9927b708f8281e4ee36be720c7f59e70
dukekon <19833038@qq.com>
2022-10-30 01:48:33 +0800

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    },
-  })],
-  esbuild: {
-    logOverride: {
-      'this-is-undefined-in-esm': 'silent',
-    },
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
